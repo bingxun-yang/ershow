10f90c5b09c5e97612ac31eefc415eaa24abfc00
bingxun.yang <bingxun.yang@lincomb.com>
2017-12-18 19:33:37 +0800

    静态文件

---
+++ ershow-web/src/test/java/test/HashTest.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * Created by Administrator on 2017/12/13.
+ */
+public class HashTest {
+    public static void main(String[] args) {
+//        String a = "123";
+//        String b = "2";
+//        System.out.println(a.hashCode());
+//        System.out.println(a.hashCode());
+//        System.out.println(b.hashCode());
+
+        System.out.println( 599 - 44);
+    }
+}
---
+++ ershow-web/src/test/java/test/SocketTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.Channel;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/12/15.
+ */
+public class SocketTest {
+
+    public static void main(String[] args) {
+//        Channel channel = new Channel(){
+//
+//            public boolean isOpen() {
+//                return false;
+//            }
+//
+//            public void close() throws IOException {
+//
+//            }
+//        };
+
+
+
+        Map<String,String> map = new HashMap<String, String>();
+
+
+
+    }
+}
